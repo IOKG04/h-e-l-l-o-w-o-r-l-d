1cdb989661fa72e94811b6277edc617f70305ceb
zxcv05 <zxcv05@tuta.com>
2024-12-11 15:08:17 -0500

    Update comments in start-build.sh

--- src/start-build.sh
+++ src/start-build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 
 cd /code/
-bash clean.sh
 
-# "tools" python module
+# build "tools" python module
 pip install --break-system-packages --root-user-action ignore -e build_tools
 
+# run main build script
 python build.py
