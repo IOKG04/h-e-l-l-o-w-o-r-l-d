868a00b5b20dbf51eadd30b195ef46f1dd07af22
zxcv05 <zxcv05@tuta.com>
2024-12-15 17:53:23 -0500

    Add generated_build.py to .gitignore (not build.py as some subprojects might use that file name)

--- src/start-build.sh
+++ src/start-build.sh
@@ -3,10 +3,10 @@
 cd /code/
 echo h-e-l-l-o-w-o-r-l-d build starting
 
-deno run --allow-read --allow-write /code/generate_build_py/generate.js /code/build.py /code/build_steps.jsonc /code/build_template.py
+deno run --allow-read --allow-write /code/generate_build_py/generate.js /code/generated_build.py /code/build_steps.jsonc /code/build_template.py
 
 # build "tools" python module
 pip install --break-system-packages --root-user-action ignore -e build_tools
 
 # run main build script
-python build.py
+python generated_build.py
