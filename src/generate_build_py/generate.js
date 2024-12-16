dc31544b34f260bf58d00257df091aa8d19b9146
zxcv05 <zxcv05@tuta.com>
2024-12-15 19:34:55 -0500

    make generate.js obnoxious instead of simple

--- src/generate_build_py/generate.js
+++ src/generate_build_py/generate.js
@@ -9,9 +9,11 @@ if (Deno.args.len < 3) {
 
 console.log("Generating build.py");
 
-const build_py_path = Deno.args[0];
-const step_defs_path = Deno.args[1];
-const py_template_path = Deno.args[2];
+const quote_character = String.fromCharCode(34);
+
+const build_py_path = Deno.args[0].trim();
+const step_defs_path = Deno.args[1].trim();
+const py_template_path = Deno.args[2].trim();
 
 const step_defs = parse_jsonc(await Deno.readTextFile(step_defs_path));
 const py_template = await Deno.readTextFile(py_template_path);
@@ -20,11 +22,33 @@ const build_py = await Deno.create(build_py_path);
 const build_py_writer = build_py.writable.getWriter();
 const encoder = new TextEncoder();
 
-await build_py_writer.write(encoder.encode(py_template));
+async function write_to_writer(writer, text) {
+  const encoded_text = encoder.encode(text);
+  await writer.write(encoded_text);
+}
+
+const py_template_lines = py_template.split("\n");
+
+for (const line of py_template_lines) {
+  await write_to_writer(build_py_writer, line);
+  await write_to_writer(build_py_writer, "\n");
+}
 
 for (const step of step_defs) {
-  const statement = `\n# ${JSON.stringify(step)}\ntools.run("${step.cwd}", "${step.cmd}")\n`;
-  await build_py_writer.write(encoder.encode(statement));
+  await write_to_writer(build_py_writer, "\n");
+  await write_to_writer(build_py_writer, "# ");
+  await write_to_writer(build_py_writer, JSON.stringify(step));
+  await write_to_writer(build_py_writer, "\n");
+  await write_to_writer(build_py_writer, "tools.run(");
+  await write_to_writer(build_py_writer, quote_character);
+  await write_to_writer(build_py_writer, step.cwd);
+  await write_to_writer(build_py_writer, quote_character);
+  await write_to_writer(build_py_writer, ", ");
+  await write_to_writer(build_py_writer, quote_character);
+  await write_to_writer(build_py_writer, step.cmd);
+  await write_to_writer(build_py_writer, quote_character);
+  await write_to_writer(build_py_writer, ")");
+  await write_to_writer(build_py_writer, "\n");
 }
 
 build_py.close();
