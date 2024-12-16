40f0d95ea1e82cd095b5f44f362b02f5d981efb0
zxcv05 <zxcv05@tuta.com>
2024-12-15 19:44:49 -0500

    Add obnoxious file path verification

--- src/generate_build_py/generate.js
+++ src/generate_build_py/generate.js
@@ -1,11 +1,14 @@
 import { parse as parse_jsonc } from "jsr:@std/jsonc@^1.0.1";
 
-if (Deno.args.len < 3) {
-  console.log(
-    "Not enough arguments. Args: <output_path> <step_defs_jsonc> <python_template>",
-  );
+function error(msg) {
+  console.log(msg);
   Deno.exit(1);
 }
+
+if (Deno.args.len < 3)
+  error(
+    "Not enough arguments. Args: <output_path> <step_defs_jsonc> <python_template>",
+  );
 
 console.log("Generating build.py");
 
@@ -14,6 +17,22 @@ const quote_character = String.fromCharCode(34);
 const build_py_path = Deno.args[0].trim();
 const step_defs_path = Deno.args[1].trim();
 const py_template_path = Deno.args[2].trim();
+
+async function verify_file_path(path) {
+  const stat = await Deno.stat(path);
+
+  if (stat.isFifo) error(`"${path}" cannot be a fifo.`);
+  if (stat.isSocket) error(`"${path}" cannot be a socket.`);
+  if (stat.isSymlink) error(`"${path}" cannot be a symlink.`); // Note: if this is actually required, just remove this
+  if (stat.isDirectory) error(`"${path}" cannot be a directory.`);
+  if (stat.isCharDevice) error(`"${path}" cannot be a char device.`);
+  if (stat.isBlockDevice) error(`"${path}" cannot be a block device.`);
+  if (stat.size == 0) error(`"${path}" is empty. Did you mean to pass that?`);
+}
+
+await verify_file_path(build_py_path);
+await verify_file_path(step_defs_path);
+await verify_file_path(py_template_path);
 
 const step_defs = parse_jsonc(await Deno.readTextFile(step_defs_path));
 const py_template = await Deno.readTextFile(py_template_path);
