d7452d505ce01fbbe853931fb120a8c3fb5dbffc
zxcv05 <zxcv05@tuta.com>
2024-12-16 17:22:00 -0500

    Add option to use "build"

--- build.sh
+++ build.sh
@@ -15,16 +15,16 @@ if [ -z "$DONT_BUILD_CONTAINER" ]; then
     log ""
 fi
 
-if [ -z "$1" ]; then CONTAINER_EXEC="time bash -e /code/start-build.sh";
+if [ -z "$1" ] || [ "$1" == "build" ]; then CONTAINER_EXEC="time bash -e /code/start-build.sh";
 elif [ "$1" == "shell" ]; then CONTAINER_EXEC="bash";
 elif [ "$1" == "clean" ]; then CONTAINER_EXEC="bash /code/clean.sh";
 elif [ "$1" == "run"   ]; then CONTAINER_EXEC="bash /code/run.sh";
 else
-    echo "$0        | Build h-e-l-l-o-w-o-r-l-d"
-    echo "$0 clean  | Remove all build related files"
-    echo "$0 run    | Run hello_world in container"
-    echo "$0 shell  | Run shell in container"
-    echo "$0 help   | Show this help message"
+    echo "$0 [build]  | Build h-e-l-l-o-w-o-r-l-d"
+    echo "$0 clean    | Remove all build related files"
+    echo "$0 run      | Run hello_world in container"
+    echo "$0 shell    | Run shell in container"
+    echo "$0 help     | Show this help message"
     exit
 fi
 
